ad66132e68d05d8216fae11c9cdd62e178eb3031
Ian Alves <gretuvinir@gmail.com>
2024-08-15 00:48:12 +0000

    codigos da aula 1

---
+++ aulas/aula01/sintaxe_c.c
@@ -0,0 +1,41 @@
+// ISSO eh diferente de isso
+// aqui eh para calcular a area do retangulo
+//area_retangulo = base * altura;
+
+
+
+
+  
+
+
+
+
+
+
+  #include <stdio.h> // inclusao de bibliotecas
+
+  int main () {    // função principal
+    // seu codigo aqui
+
+    //1. entrada
+    // levantar meus ingredientes
+    // char, int, float
+    // variaveis
+    // leitura
+    int primeiro_numero;
+    int segundo_numero;
+    primeiro_numero = 706461;
+    segundo_numero = 937570;
+    
+    //2. processamento
+    // calculo soma, subtraçao, multiplicaçao, divisao
+    int soma = primeiro_numero + segundo_numero;
+    
+    //3. saida
+    // escrita
+    printf("A soma de %i com %i eh %i\n", primeiro_numero, segundo_numero, soma);
+    
+    return 0;      // return fim do programa
+    
+
+}
